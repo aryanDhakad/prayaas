1c3610f6e1d07e7b4f15162cd18f5f12d4bad5dc
Aryan Dhakad <aryandhakad1@gmail.com>
2021-01-14 09:26:59 +0530

    added blog page and blog cards, added modal for event card and blog card, updated footer

--- index.js
+++ index.js
@@ -1,122 +1,131 @@
-$(window).on("load",function(){
-     $(".loader-wrapper").delay(1500).fadeOut("fast");
+$(window).on("load", function () {
+  $(".loader-wrapper").delay(1500).fadeOut("fast");
 });
 
 const faders = document.querySelectorAll(".fade-in");
 const sliders = document.querySelectorAll(".slide-in");
 
 const appearOptions = {
-    threshold: 0,
-    rootMargin: "0px 0px -250px 0px"
+  threshold: 0,
+  rootMargin: "0px 0px -250px 0px",
 };
 
 const appearOnScroll = new IntersectionObserver(function (
-        entries,
-        appearOnScroll
-    ) {
-        entries.forEach(entry => {
-            if (!entry.isIntersecting) {
-                return;
-            } else {
-                entry.target.classList.add("appear");
-                appearOnScroll.unobserve(entry.target);
-            }
-        });
-    },
-    appearOptions);
+  entries,
+  appearOnScroll
+) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add("appear");
+      appearOnScroll.unobserve(entry.target);
+    }
+  });
+},
+appearOptions);
 
-faders.forEach(fader => {
-    appearOnScroll.observe(fader);
+faders.forEach((fader) => {
+  appearOnScroll.observe(fader);
 });
 
-sliders.forEach(slider => {
-    appearOnScroll.observe(slider);
+sliders.forEach((slider) => {
+  appearOnScroll.observe(slider);
 });
-
 
 // LOADER JS
 function fitElementToParent(el, padding) {
-    var timeout = null;
-    function resize() {
-      if (timeout) clearTimeout(timeout);
-      anime.set(el, {scale: 1});
-      var pad = padding || 0;
-      var parentEl = el.parentNode;
-      var elOffsetWidth = el.offsetWidth - pad;
-      var parentOffsetWidth = parentEl.offsetWidth;
-      var ratio = parentOffsetWidth / elOffsetWidth;
-      timeout = setTimeout(anime.set(el, {scale: ratio}), 10);
-    }
-    resize();
-    window.addEventListener('resize', resize);
+  var timeout = null;
+  function resize() {
+    if (timeout) clearTimeout(timeout);
+    anime.set(el, { scale: 1 });
+    var pad = padding || 0;
+    var parentEl = el.parentNode;
+    var elOffsetWidth = el.offsetWidth - pad;
+    var parentOffsetWidth = parentEl.offsetWidth;
+    var ratio = parentOffsetWidth / elOffsetWidth;
+    timeout = setTimeout(anime.set(el, { scale: ratio }), 10);
   }
-  
-  var sphereAnimation = (function() {
-  
-    var sphereEl = document.querySelector('.sphere-animation');
-    var spherePathEls = sphereEl.querySelectorAll('.sphere path');
-    var pathLength = spherePathEls.length;
-    var hasStarted = false;
-    var aimations = [];
-  
-    fitElementToParent(sphereEl);
-  
-    var breathAnimation = anime({
-      begin: function() {
-        for (var i = 0; i < pathLength; i++) {
-          aimations.push(anime({
+  resize();
+  window.addEventListener("resize", resize);
+}
+
+var sphereAnimation = (function () {
+  var sphereEl = document.querySelector(".sphere-animation");
+  var spherePathEls = sphereEl.querySelectorAll(".sphere path");
+  var pathLength = spherePathEls.length;
+  var hasStarted = false;
+  var aimations = [];
+
+  fitElementToParent(sphereEl);
+
+  var breathAnimation = anime({
+    begin: function () {
+      for (var i = 0; i < pathLength; i++) {
+        aimations.push(
+          anime({
             targets: spherePathEls[i],
-            stroke: {value: ['rgba(255,75,75,1)', 'rgba(80,80,80,.35)'], duration: 500},
+            stroke: {
+              value: ["rgba(255,75,75,1)", "rgba(80,80,80,.35)"],
+              duration: 500,
+            },
             translateX: [2, -4],
             translateY: [2, -4],
-            easing: 'easeOutQuad',
-            autoplay: false
-          }));
-        }
-      },
-      update: function(ins) {
-        aimations.forEach(function(animation, i) {
-          var percent = (1 - Math.sin((i * .35) + (.0022 * ins.currentTime))) / 2;
-          animation.seek(animation.duration * percent);
-        });
-      },
-      duration: Infinity,
-      autoplay: false
-    });
-  
-    var introAnimation = anime.timeline({
-      autoplay: false
+            easing: "easeOutQuad",
+            autoplay: false,
+          })
+        );
+      }
+    },
+    update: function (ins) {
+      aimations.forEach(function (animation, i) {
+        var percent = (1 - Math.sin(i * 0.35 + 0.0022 * ins.currentTime)) / 2;
+        animation.seek(animation.duration * percent);
+      });
+    },
+    duration: Infinity,
+    autoplay: false,
+  });
+
+  var introAnimation = anime
+    .timeline({
+      autoplay: false,
     })
-    .add({
-      targets: spherePathEls,
-      strokeDashoffset: {
-        value: [anime.setDashoffset, 0],
-        duration: 3900,
-        easing: 'easeInOutCirc',
-        delay: anime.stagger(190, {direction: 'reverse'})
+    .add(
+      {
+        targets: spherePathEls,
+        strokeDashoffset: {
+          value: [anime.setDashoffset, 0],
+          duration: 3900,
+          easing: "easeInOutCirc",
+          delay: anime.stagger(190, { direction: "reverse" }),
+        },
+        duration: 2000,
+        delay: anime.stagger(60, { direction: "reverse" }),
+        easing: "linear",
       },
-      duration: 2000,
-      delay: anime.stagger(60, {direction: 'reverse'}),
-      easing: 'linear'
-    }, 0);
-  
-    var shadowAnimation = anime({
-        targets: '#sphereGradient',
-        x1: '25%',
-        x2: '25%',
-        y1: '0%',
-        y2: '75%',
-        duration: 30000,
-        easing: 'easeOutQuint',
-        autoplay: false
-      }, 0);
-  
-    function init() {
-      introAnimation.play();
-      breathAnimation.play();
-      shadowAnimation.play();
-    }
-    
-    init();
-  
-  })();
+      0
+    );
+
+  var shadowAnimation = anime(
+    {
+      targets: "#sphereGradient",
+      x1: "25%",
+      x2: "25%",
+      y1: "0%",
+      y2: "75%",
+      duration: 30000,
+      easing: "easeOutQuint",
+      autoplay: false,
+    },
+    0
+  );
+
+  function init() {
+    introAnimation.play();
+    breathAnimation.play();
+    shadowAnimation.play();
+  }
+
+  init();
+})();
